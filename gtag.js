e27c2cfa1729f03a9d5739bcc80d657e47b35b0f
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2021-09-11 23:50:06 +0200

    association

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=286095790"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '286095790');
+</script>
